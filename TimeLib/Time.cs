ed6f323685fffad7411e96fedede746413659ffa
Dominik Zemlik <dominik.zemlik.dev@gmail.com>
2023-04-20 22:26:10 +0200

    Create constructors, implement IEquatable and IComparable for Time and TimePeriod structures

--- TimeLib/Time.cs
+++ TimeLib/Time.cs
@@ -1,7 +1,80 @@
 ﻿namespace TimeLib
 {
-    public readonly struct Time
+    public readonly struct Time : IEquatable<Time>, IComparable<Time>
     {
+        public readonly byte Hours { get; }
 
+        public readonly byte Minutes { get; }
+
+        public readonly byte Seconds { get; }
+
+        #region ===== Constructors =====
+
+        public Time(byte hours, byte minutes, byte seconds)
+        {
+            if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
+                throw new ArgumentOutOfRangeException();
+
+            Hours = hours;
+            Minutes = minutes;  
+            Seconds = seconds;  
+        }
+
+        public Time(byte hours, byte minutes): this(hours, minutes, 0) { }
+    
+        public Time(byte hours): this(hours, 0, 0) { }
+
+        public Time(): this((byte)DateTime.Now.Hour, (byte)DateTime.Now.Minute, (byte)DateTime.Now.Second) { }
+
+        #endregion
+
+        #region ===== Equatable =====
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+
+            if (obj is Time)
+            {
+                return Equals((Time)obj);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Time other) => Hours == other.Hours && Minutes == other.Minutes && Seconds == other.Seconds;
+
+        public override int GetHashCode() => (Hours, Minutes, Seconds).GetHashCode();
+
+        public static bool operator ==(Time left, Time right) => left.Equals(right);
+
+        public static bool operator !=(Time left, Time right) => !left.Equals(right);
+
+        #endregion
+
+        #region ===== Comparable =====
+
+        public int CompareTo(Time other)
+        {
+            if (Hours != other.Hours)
+                return Hours.CompareTo(other.Hours);
+            
+            if (!Minutes.Equals(other.Minutes))
+                return Minutes.CompareTo(other.Minutes);
+
+            return Seconds.CompareTo(other.Seconds);
+        }
+
+        public static bool operator >(Time left, Time right) => left.CompareTo(right) > 0;
+
+        public static bool operator <(Time left, Time right) => left.CompareTo(right) < 0;
+
+        public static bool operator >=(Time left, Time right) => left.CompareTo(right) >= 0;
+
+        public static bool operator <=(Time left, Time right) => left.CompareTo(right) <= 0;
+
+        #endregion
+
+        public override string ToString() => $"{Hours:00}:{Minutes:00}:{Seconds:00}";
     }
 }
--- TimeLib/TimePeriod.cs
+++ TimeLib/TimePeriod.cs
@@ -1,7 +1,72 @@
 ﻿namespace TimeLib
 {
-    public readonly struct TimePeriod
+    public readonly struct TimePeriod : IEquatable<TimePeriod>, IComparable<TimePeriod>
     {
+        public readonly long Seconds { get; }
+
+        #region ===== Constructors =====
+
+        public TimePeriod(long hours, long minutes, long seconds)
+        {
+            if(hours < 0 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
+                throw new ArgumentOutOfRangeException();
+
+            Seconds = (hours * 3600) + (minutes * 60) + seconds;
+        }
+
+        public TimePeriod(long hours, long minutes): this(hours, minutes, 0) { }
+
+        public TimePeriod(long seconds): this(0, 0, seconds) { }
+
+        public TimePeriod(Time left, Time right)
+        {
+            long leftTotalSeconds = (left.Hours * 3600) + (left.Minutes * 60) + left.Seconds;
+            long rightTotalSeconds = (right.Hours * 3600) + (right.Minutes * 60) + right.Seconds;
+
+            Seconds = leftTotalSeconds >= rightTotalSeconds ? leftTotalSeconds - rightTotalSeconds : rightTotalSeconds - leftTotalSeconds;
+        }
+
+        #endregion
+
+        #region ===== Equatable =====
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+
+            if (obj is TimePeriod)
+            {
+                return Equals((TimePeriod)obj);
+            }
+
+            return false;
+        }
+
+        public bool Equals(TimePeriod other) => Seconds == other.Seconds;
+
+        public override int GetHashCode() => (Seconds).GetHashCode();
+
+        public static bool operator ==(TimePeriod left, TimePeriod right) => left.Equals(right);
+
+        public static bool operator !=(TimePeriod left, TimePeriod right) => !left.Equals(right);
+
+        #endregion
+
+        #region ===== Comparable =====
+
+        public int CompareTo(TimePeriod other) => Seconds.CompareTo(other.Seconds);
+     
+        public static bool operator >(TimePeriod left, TimePeriod right) => left.CompareTo(right) > 0;
+
+        public static bool operator <(TimePeriod left, TimePeriod right) => left.CompareTo(right) < 0;
+
+        public static bool operator >=(TimePeriod left, TimePeriod right) => left.CompareTo(right) >= 0;
+
+        public static bool operator <=(TimePeriod left, TimePeriod right) => left.CompareTo(right) <= 0;
+
+        #endregion
+
+        public override string ToString() => $"{Seconds / 3600}:{(Seconds / 60) % 60:D2}:{Seconds % 60:D2}";
 
     }
 }
