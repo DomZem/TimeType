f723f4469dbc55cadcca954702368c27849c0431
Dominik <dominik.zemlik.dev@gmail.com>
2023-05-01 22:04:06 +0200

    Add XML documentation for Time class

--- TimeLib/Time.cs
+++ TimeLib/Time.cs
@@ -4,14 +4,30 @@ namespace TimeLib
 {
     public readonly struct Time : IEquatable<Time>, IComparable, IComparable<Time>
     {
+        /// <summary>
+        /// Gets the number of hours in the time.
+        /// </summary>
         public readonly byte Hours { get; }
 
+        /// <summary>
+        /// Gets the number of minutes in the time.
+        /// </summary>
         public readonly byte Minutes { get; }
 
+        /// <summary>
+        /// Gets the number of seconds in the time.
+        /// </summary>
         public readonly byte Seconds { get; }
 
         #region ===== Constructors =====
 
+        /// <summary>
+        /// Creates a new Time object with the specified hours, minutes, and seconds. 
+        /// </summary>
+        /// <param name="hours">The number of hours in the time. Must be between 0 and 23.</param>
+        /// <param name="minutes">The number of minutes in the time. Must be between 0 and 59.</param>
+        /// <param name="seconds">The number of seconds in the time. Must be between 0 and 59.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if any of the parameters are out of range.</exception>
         public Time(byte hours, byte minutes, byte seconds)
         {
             if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
@@ -22,10 +38,24 @@ namespace TimeLib
             Seconds = seconds;  
         }
 
+        /// <summary>
+        /// Creates a new Time object with the specified hours and minutes. The seconds are set to 0.
+        /// </summary>
+        /// <param name="hours">The number of hours in the time. Must be between 0 and 23.</param>
+        /// <param name="minutes">The number of hours in the time. Must be between 0 and 23.</param>
         public Time(byte hours, byte minutes): this(hours, minutes, 0) { }
-    
+
+        /// <summary>
+        /// Creates a new Time object with the specified number of hours. The minutes and seconds are set to 0.
+        /// </summary>
+        /// <param name="hours">The number of hours in the time. Must be between 0 and 23.</param>
         public Time(byte hours): this(hours, 0, 0) { }
-        
+
+        /// <summary>
+        /// Creates a new Time object from a string in the format "hh:mm:ss".
+        /// </summary>
+        /// <param name="timeText">The time string to parse. Must be in the format "hh:mm:ss".</param>
+        /// <exception cref="ArgumentException">Thrown when the time string is null, empty or not in the correct format.</exception>
         public Time(string timeText)
         {
             if (String.IsNullOrEmpty(timeText))
@@ -45,12 +75,15 @@ namespace TimeLib
         }
 
         // Extra
+        /// <summary>
+        /// Creates a new Time object initialized to the current system time.
+        /// </summary>
         public Time(): this((byte)DateTime.Now.Hour, (byte)DateTime.Now.Minute, (byte)DateTime.Now.Second) { }
 
         #endregion
 
         #region ===== Equatable =====
-
+     
         public override bool Equals(object? obj)
         {
             if (obj is null) return false;
@@ -61,6 +94,11 @@ namespace TimeLib
             return false;
         }
 
+        /// <summary>
+        /// Determines whether this <see cref="Time"/> instance is equal to another <see cref="Time"/> instance.
+        /// </summary>
+        /// <param name="other">The <see cref="Time"/> to compare with this instance.</param>
+        /// <returns><c>true</c> if the two instances have the same number of hours, minutes and seconds; otherwise, <c>false</c>.</returns>
         public bool Equals(Time other) => Hours == other.Hours && Minutes == other.Minutes && Seconds == other.Seconds;
 
         public override int GetHashCode() => (Hours, Minutes, Seconds).GetHashCode();
@@ -73,6 +111,18 @@ namespace TimeLib
 
         #region ===== Comparable =====
 
+        /// <summary>
+        /// Compares this Time object with another object, returning an integer that indicates the relationship between the two objects.
+        /// </summary>
+        /// <param name="obj">The object to compare to this Time object.</param>
+        /// <returns>
+        /// A value less than zero if this Time object is less than the <paramref name="obj"/> argument,
+        /// zero if this Time object is equal to the <paramref name="obj"/> argument, and a value greater
+        /// than zero if this Time object is greater than the <paramref name="obj"/> argument.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the <paramref name="obj"/> argument is not a Time object.
+        /// </exception>
         public int CompareTo(object? obj)
         {
             if (obj is null) return +1;
@@ -85,6 +135,11 @@ namespace TimeLib
             return this.CompareTo(other);
         }
 
+        /// <summary>
+        /// Compares the current <see cref="Time"/> object with another <see cref="Time"/> object and returns an integer that indicates their relative positions in the sort order.
+        /// </summary>
+        /// <param name="other">The <see cref="Time"/> object to compare with the current object.</param>
+        /// <returns>A signed integer that indicates the relative position of the current object and the <paramref name="other"/> parameter in the sort order.</returns>
         public int CompareTo(Time other)
         {
             if (Hours != other.Hours)
@@ -106,6 +161,10 @@ namespace TimeLib
 
         #endregion
 
+        /// <summary>
+        /// Returns the string representation of this Time object in the format "hh:mm:ss".
+        /// </summary>
+        /// <returns>The string representation of this Time object.</returns>
         public override string ToString() => $"{Hours:00}:{Minutes:00}:{Seconds:00}";
     }
 }
